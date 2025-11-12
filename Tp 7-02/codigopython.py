158f809a32a59e1ad9f080ab9e7fba1148af00ab
LautQuinteros <lautaroquinteros087@gmail.com>
2025-11-11 23:51:45 -0300

    11-11

--- Tp 7-02/codigopython.py
+++ Tp 7-02/codigopython.py
@@ -11,24 +11,24 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
 # --- Parámetros iniciales ---
-F0 = 1000          # Fuerza inicial [Kg]
-L0 = 3             # Longitud inicial [m]
-delta_max = 1e-2   # Deflexión máxima permitida [m]
+F = 1000          # Fuerza inicial [Kg]
+L = 3             # Longitud inicial [m]
+flecha = 1e-2   # flecha máxima permitida [m]
 
 # Lista de materiales Genéricos
 materiales = [
-    ('Acero',    210e9, 7850),
-    ('Aluminio',  70e9, 2700),
-    ('Titanio',  115e9, 4500),
-    ('Madera',    11e9,  600),
-    ('Polimero',   2e9, 1200)
+    ('Acero F24',          210e9, 7850),
+    ('Aluminio 6063',      69e9, 2700),
+    ('Titanio AlyV',       114e9, 4430),
+    ('Madera de roble',    11e9,  770),
+    ('Teflón',             2e9, 2200)
 ]
-
+# --- Función de cálculo ---
 def calcular_resultados(F, L):
     """Calcula espesor, masa e índice de Ashby para cada material."""
     resultados = []
     for nombre, E, rho in materiales:
-        t = (4 * F * 10 * L**3 / (E * delta_max))**0.25
+        t = (4 * F * 10 * L**3 / (E * flecha))**0.25
         m = rho * L * t**2
         M = np.sqrt((rho ^ 2) / (E))
         resultados.append((nombre, t, m, M))
@@ -37,23 +37,23 @@ def calcular_resultados(F, L):
     return df
 
 # --- Cálculo inicial ---
-T = calcular_resultados(F0, L0)
+T = calcular_resultados(F, L)
 
 # --- Figura y gráfico ---
 fig, ax = plt.subplots(figsize=(8,5))
-plt.subplots_adjust(bottom=0.25)  # deja espacio para sliders
+plt.subplots_adjust(bottom=0.25)  
 
 bars = ax.bar(T['Material'], T['Indice_Ashby'])
-ax.set_ylabel(r'Índice de Ashby $E^{1/2} / \rho$')
-ax.set_title(f'Eficiencia de materiales (F={F0:.0f} Kg, L={L0:.2f} m)')
+ax.set_ylabel(r'Índice de Ashby $\rho^{2} / E$')
+ax.set_title(f'Eficiencia de materiales (F={F:.0f} Kg, L={L:.2f} m)')
 ax.grid(True, axis='y', linestyle='--', alpha=0.7)
 
 # --- Sliders ---
 ax_F = plt.axes([0.2, 0.10, 0.65, 0.03])
 ax_L = plt.axes([0.2, 0.05, 0.65, 0.03])
 
-slider_F = Slider(ax_F, 'F [Kg]', 100, 5000, valinit=F0, valstep=100)
-slider_L = Slider(ax_L, 'L [m]', 1, 6, valinit=L0, valstep=0.1)
+slider_F = Slider(ax_F, 'F [Kg]', 100, 10000, valinit=F, valstep=100)
+slider_L = Slider(ax_L, 'L [m]', 1, 20, valinit=L, valstep=0.1)
 
 # --- Función de actualización ---
 def actualizar(val):
@@ -72,7 +72,6 @@ def actualizar(val):
     print("\n=== Resultados ordenados por masa mínima ===")
     print(Tn.to_string(index=False, justify='center', col_space=12))
     mejor = Tn.loc[0, 'Material']
-    print(f"\n👉 El material más eficiente en peso (menor masa) es: {mejor}")
 
 # Conectar sliders
 slider_F.on_changed(actualizar)
